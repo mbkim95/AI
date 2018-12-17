4c1210bdf5d6e31cf31a2a766957d86dde076c01
mbkim95 <mbkim95@naver.com>
2018-12-17 10:31:00 +0900

    Algorithm
    Implementation

--- main.py
+++ main.py
@@ -8,23 +8,28 @@ from DataSet import *
 class NeuralNet(nn.Module):
     def __init__(self, input_size, output_size):
         super(NeuralNet, self).__init__()
-        self.input_layer = nn.Linear(input_size, 20)
-        # self.L1 = nn.Linear(120, 100)
-        self.output_layer = nn.Linear(20, output_size)
+        self.input_layer = nn.Linear(input_size, 120)
+        self.L1 = nn.Linear(120, 100)
+        self.output_layer = nn.Linear(100, output_size)
 
     def forward(self, x):
         x = F.relu(self.input_layer(x))
-        # x = F.relu(self.L1(x))
+        x = F.relu(self.L1(x))
         x = self.output_layer(x)
         return x
 
-def init_weights(m):                                        # 초기 Weights 설정
-    if isinstance(m, nn.Linear):
-        size = m.weight.size()
-        fan_out = size[0]  # number of rows
-        fan_in = size[1]  # number of columns
-        variance = np.sqrt(2.0 / (fan_in + fan_out))
-        m.weight.data.normal_(0.0, variance)
+# def init_weights(m):                                        # 초기 Weights 설정
+#     if isinstance(m, nn.Linear):
+#         size = m.weight.size()
+#         fan_out = size[0]  # number of rows
+#         fan_in = size[1]  # number of columns
+#         variance = np.sqrt(2.0 / (fan_in + fan_out))
+#         m.weight.data.normal_(0.0, variance)
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
 
 def train(model, input_data, target_data, optimizer, epoch):
     # In training progress
@@ -54,42 +59,38 @@ def train(model, input_data, target_data, optimizer, epoch):
 
 def test(model, input_test, input_target):
     model.eval()
-    correct = 0
+    # correct = 0
     for data, target in zip(input_test, input_target):
         output_data = model(data)
-        value = torch.round(output_data).long()
+        target = target.float()
+        l = output_data
+        value = output_data
+        # value = torch.round(output_data).long()
         # value = value.long()
-        if value == target:
-            correct += 1
-    percentage = round(100 * (correct / len(input_target)))
-    print('Percentage:{}%({}/{})'.format(percentage, correct, len(input_target)))
-
-def divide_Dataset(input, target, device, batch_size, i):
-    train_values, test_values = [], []
-    for idx, (x_test, y_test) in range(i, i+batch_size) and enumerate(zip(input, target)):
-        x_train = torch.Tensor([], device=device)
-        y_train = torch.LongTensor([], device=device)
-        for train_idx, (x, y) in enumerate(zip(input, target)):
-            if train_idx != idx:
-                x_train = torch.cat((x_train, x), 0)  # x_train과 x을 이어붙임
-                y_train = torch.cat((y_train, y), 0)  # y_train과 y를 이어붙임
-                return x_train, y_train
+        criterion = nn.MSELoss()
+        loss = criterion(l, target)
+        correct = 1 - abs(value - target)/21
+        # if value == t:
+        #     correct += 1
+    # percentage = round(100 * (correct / len(input_target)))
+    print('{} {} {}'.format(value, target, correct))
+    percentage = correct * 100
+    percentage = torch.squeeze(percentage)
+    # print('Percentage:{}%({}/{})'.format(percentage, correct, len(input_target)))
+    print('Percentage:{}%'.format(percentage))
+    print('Loss:{}'.format(loss))
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # 1. Model representation
     train_input, train_target, test_input, test_target, col = preprocess('./student/student-por.csv')
-    ratio = 10
-
-    # if you want cross validation
-    # input, target = cross_validation(ratio, input_data, target_data)
 
     ##
     model = NeuralNet(col, 1).to(device)
-    # model.apply(init_weights)
+    model.apply(init_weights)
     # 2. Setup optimizer
     optimizer = optim.SGD(model.parameters(), lr=0.001)
-    epoch_range, batch = 1000, 5
+    epoch_range, batch = 500, 5
 
     #
     for epoch in range(0, epoch_range):
@@ -98,20 +99,6 @@ def main():
         # 4. Evaluate the training model
     test(model, test_input, test_target)
 
-
-    #
-    # train_values, test_values = [], []
-    # for idx, (x_test, y_test) in enumerate(zip(input, target)):
-    #     x_train = torch.Tensor([], device=device)
-    #     y_train = torch.LongTensor([], device=device)
-    #     for train_idx, (x, y) in enumerate(zip(input, target)):
-    #         if train_idx != idx:
-    #             x_train = torch.cat((x_train, x), 0)  # x_train과 x을 이어붙임
-    #             y_train = torch.cat((y_train, y), 0)  # y_train과 y를 이어붙임
-    #
-    #             train(model, x_train, y_train, optimizer, idx)
-    #
-    #             test(model, x_test, y_test)
 
 if __name__ == '__main__':
     main()
